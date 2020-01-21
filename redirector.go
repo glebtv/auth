0282540b0d65fe2bb866591326befa391eccbc4e
Gleb Tv <glebtv@gmail.com>
2020-01-21 12:22:13 +0300

    log

--- redirector.go
+++ redirector.go
@@ -20,6 +20,6 @@ type Redirector struct {
 
 // Redirect redirect back after action
 func (redirector Redirector) Redirect(w http.ResponseWriter, req *http.Request, action string) {
-	log.Println("redirect back", action)
+	log.Println("redirect back to action", action)
 	redirector.RedirectBack.RedirectBack(w, req)
 }
